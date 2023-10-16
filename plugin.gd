d1f4102689aaf7c580e3a7fc57cec53c4fba0e72
20ml <milliliter.avc@gmail.com>
2023-10-16 15:21:39 -0400

    Add console keybind ProjectSetting and registration on run

--- plugin.gd
+++ plugin.gd
@@ -1,8 +1,30 @@
 @tool
+class_name CommandTerminalPlugin
 extends EditorPlugin
 
-func _enter_tree():
-	add_autoload_singleton("CommandServer", "res://addons/command-terminal/src/CommandServer.gd")
+var _console_key: InputEventKey = preload("res://addons/command-terminal/ast/default_console_key.tres").duplicate()
 
-func _exit_tree():
-	remove_autoload_singleton(name)
+const PLUGIN_ID := "command_terminal"
+const PLUGIN_PATH := "plugins/" + PLUGIN_ID
+const CONSOLE_KEY_SHORTCUT := PLUGIN_PATH + "/console_key_shortcut"
+
+func _enable_plugin():
+	add_autoload_singleton("CommandServer", "res://addons/command-terminal/src/CommandServer.gd")
+	_set_setting_or_default(CONSOLE_KEY_SHORTCUT, _console_key)
+	_set_setting_info({
+		"name" : CONSOLE_KEY_SHORTCUT,
+		"type" : TYPE_OBJECT,
+	})
+
+func _disable_plugin():
+	remove_autoload_singleton("CommandServer")
+
+func _set_setting_or_default(setting_name: String, value: Variant) -> void:
+	if not ProjectSettings.has_setting(setting_name):
+		ProjectSettings.set_setting(setting_name, value)
+	ProjectSettings.set_initial_value(setting_name, value)
+
+func _set_setting_info(info: Dictionary) -> void:
+	ProjectSettings.add_property_info(info)
+
+
